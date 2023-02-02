537f6f0b9c3e6c38e4e8f01ec0ed592ad31a1783
Ritik Goyal <ritik.goyal@cogoport.com>
2023-02-02 22:15:35 +0530

    commit 2(upto chaining)

--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@ import React, { useState,useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import '../src/index.css'
 import {v4 as uuid} from 'uuid';
+import {ImBin2} from "react-icons/im"
+import {BsCheckSquare} from "react-icons/bs"
 const App = () => {
   
   const [category,setCategory] = useState('default');
   const [todo,setTodo] = useState('');
   const [date,setDate] = useState('1999-09-15');
   const [todos,setTodos] = useState([]);
+  const [showtodos,setShowtodos] = useState(todos);
   const [search,setSearch] = useState('');
+  const [filter,setFilter] = useState('');
+  const [datefilter,setDateFilter] = useState('');
+  
   useEffect(() => {
     const items = JSON.parse(localStorage.getItem('items'));
     if(items){
@@ -18,12 +24,30 @@ const App = () => {
 
   useEffect(() => {
     localStorage.setItem('items', JSON.stringify(todos));
+    setShowtodos(todos);
   }, [todos]);
-  
-  
+
+  useEffect(() => {
+    // if(showtodos.length==todos.length){
+      setShowtodos(todos.filter(t => t.text.includes(search)));
+    // }else{
+    //   setShowtodos(showtodos.filter(t => t.text.includes(search)));
+    // }
+  }, [search]);
+
+  useEffect(() => {
+    // console.log(filter);
+    // console.log(showtodos);
+    setShowtodos(todos.filter(t => t.filter===filter));
+    if(filter==="all") setShowtodos(todos);
+  }, [filter]);
+  useEffect(() => {
+    setShowtodos(todos.filter(t => t.date===datefilter));
+  }, [datefilter]);
+
   const addTodo = () => {
     const id = uuid();
-    setTodos([...todos,{id:id,text:todo,status:false,category:category,date:date}]);
+    setTodos([...todos,{id:id,text:todo,status:false,category:category,date:date,filter:category}]);
     setTodo("");
   }
   const deleteTodo = (id) => {
@@ -42,12 +66,27 @@ const App = () => {
     <main>
       <div className='container'>
         <h1>To-Do List</h1>
-        <input
-        value={search}
-        className='search'
-        onChange={(e) => setSearch(e.target.value)}
-        placeholder='Search Here'
-        type="text" />
+        <div className="search-and-filter">
+          
+          <input
+          value={search}
+          className='search'
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder='Search Here'
+          type="text" />
+          <div className="filters">
+            <span>Filter By :-</span>
+            <input type="date" onChange={(e)=>setDateFilter(e.target.value)} />
+            <select name="category" id="category" onChange={(e)=>setFilter(e.target.value)}>
+            <option value="all">All Category</option>
+            <option value="default">Default</option>
+            <option value="food">Food</option>
+            <option value="travel">Travel</option>
+            <option value="fitness">Fitness</option>
+          </select>
+
+          </div>
+        </div>
         <div className='add-container'>
         <input
         placeholder='Enter To-Do here'
@@ -68,6 +107,22 @@ const App = () => {
         </div>
       <div className='todos-container'>
         {
+          showtodos.map((todo) => {
+            return <div className={`todo-item ${todo.status && `strikethrough`}`}>
+                     <BsCheckSquare className={`check ${todo.status && `checked`}`} onClick={() => {markDone(todo.id)}} />
+                     <div className='todo-details'>
+                      <span className='todo-text'>{todo.status===true?<s>{todo.text}</s>:todo.text}</span>
+                      <div className="date-category">
+                        <span className='category'>{todo.status===true?<s>{todo.category}</s>:todo.category}</span>
+                        <span className='date'>{todo.status===true?<s>{todo.date}</s>:todo.date}</span>
+                      </div>
+                     </div>
+                     <ImBin2 className='delete-button' onClick={() => deleteTodo(todo.id)} />
+                     {/* <button className='delete-button' onClick={() => deleteTodo(todo.id)}>Delete</button> */}
+                   </div>
+          })
+        }
+        {/* {
           search===""?(todos.map((todo) => {
             return <div className='todo-item'>
                      <input className='check' type="checkbox" onChange={() => {markDone(todo.id)}} />
@@ -76,7 +131,8 @@ const App = () => {
                       <span className='category'>{todo.category}</span>
                       <span className='date'>{todo.date}</span>
                      </div>
-                     <button className='delete-button' onClick={() => deleteTodo(todo.id)}>Delete</button>
+                     <ImBin2 className='delete-button' onClick={() => deleteTodo(todo.id)} />
+                     
                    </div>
           })):(todos.map((todo) => {
             if(todo.text.includes(search)){
@@ -87,11 +143,14 @@ const App = () => {
                       <span className='category'>{todo.category}</span>
                       <span className='date'>{todo.date}</span>
                      </div>
-              <button className='delete-button' onClick={() => deleteTodo(todo.id)}>Delete</button>
+                     <ImBin2 className='delete-button' onClick={() => deleteTodo(todo.id)} />
+
             </div>
             }
           }))
-        }
+
+          // console.log("hello")
+        } */}
       </div>
       </div>
     </main>
