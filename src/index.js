0314f226ad8a4f6d387e64facb75942a4fd2f493
Ritik Goyal <ritik.goyal@cogoport.com>
2023-02-02 17:08:18 +0530

    commit 1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,102 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState,useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import '../src/index.css'
+import {v4 as uuid} from 'uuid';
+const App = () => {
+  
+  const [category,setCategory] = useState('default');
+  const [todo,setTodo] = useState('');
+  const [date,setDate] = useState('1999-09-15');
+  const [todos,setTodos] = useState([]);
+  const [search,setSearch] = useState('');
+  useEffect(() => {
+    const items = JSON.parse(localStorage.getItem('items'));
+    if(items){
+      setTodos(items);
+    }
+  }, []);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  useEffect(() => {
+    localStorage.setItem('items', JSON.stringify(todos));
+  }, [todos]);
+  
+  
+  const addTodo = () => {
+    const id = uuid();
+    setTodos([...todos,{id:id,text:todo,status:false,category:category,date:date}]);
+    setTodo("");
+  }
+  const deleteTodo = (id) => {
+    setTodos(todos.filter(t => t.id!== id));
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const markDone = (id) => {
+    setTodos(
+      todos.map(t => {
+        if(t.id==id) t.status = !t.status;
+        return t;
+      })
+    )
+  }
+  return(
+    <main>
+      <div className='container'>
+        <h1>To-Do List</h1>
+        <input
+        value={search}
+        className='search'
+        onChange={(e) => setSearch(e.target.value)}
+        placeholder='Search Here'
+        type="text" />
+        <div className='add-container'>
+        <input
+        placeholder='Enter To-Do here'
+        value = {todo}
+        onChange={(e) => setTodo(e.target.value)}
+        type="text" />
+        <div className='category-container'>
+          <input type="date" name="" id="" onChange={(e)=>setDate(e.target.value)}/>
+          <select name="category" id="category" onChange={(e)=>setCategory(e.target.value)}>
+            <option value="default">Default</option>
+            <option value="food">Food</option>
+            <option value="travel">Travel</option>
+            <option value="fitness">Fitness</option>
+          </select>
+        </div>
+
+          <button className='add-button' onClick={addTodo}>ADD</button>
+        </div>
+      <div className='todos-container'>
+        {
+          search===""?(todos.map((todo) => {
+            return <div className='todo-item'>
+                     <input className='check' type="checkbox" onChange={() => {markDone(todo.id)}} />
+                     <div className='todo-details'>
+                      <span>{todo.status===true?<s>{todo.text}</s>:todo.text}</span>
+                      <span className='category'>{todo.category}</span>
+                      <span className='date'>{todo.date}</span>
+                     </div>
+                     <button className='delete-button' onClick={() => deleteTodo(todo.id)}>Delete</button>
+                   </div>
+          })):(todos.map((todo) => {
+            if(todo.text.includes(search)){
+              return <div className='todo-item'>
+              <input className='check' type="checkbox" onChange={() => {markDone(todo.id)}} />
+              <div className='todo-details'>
+                      <span>{todo.status===true?<s>{todo.text}</s>:todo.text}</span>
+                      <span className='category'>{todo.category}</span>
+                      <span className='date'>{todo.date}</span>
+                     </div>
+              <button className='delete-button' onClick={() => deleteTodo(todo.id)}>Delete</button>
+            </div>
+            }
+          }))
+        }
+      </div>
+      </div>
+    </main>
+  )
+}
+
+ReactDOM.render(<App />,document.getElementById('root'));
+
